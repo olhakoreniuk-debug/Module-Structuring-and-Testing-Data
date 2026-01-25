a80c47473a621890b94360bf204e733c370e3425
olhakoreniuk-debug <olha.koreniuk@gmail.com>
2026-01-25 17:43:51 +0100

    1-key-exercises from Sprint-1 done!

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -2,6 +2,16 @@ const minimum = 1;
 const maximum = 100;
 
 const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+const rangeSize = maximum - minimum + 1;
+const random0to1 = Math.random();
+const scaled = random0to1 * rangeSize;
+const floored = Math.floor(scaled);
+const finalNum = floored + minimum;
+
+console.log({ minimum, maximum, rangeSize });
+console.log({ random0to1, scaled, floored, finalNum });
+// num is a random integer between minimum and maximum (inclusive).
+// Math.random() gives [0, 1), scaling gives [0, rangeSize), floor makes an integer, +minimum shifts to [minimum, maximum].
 
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
