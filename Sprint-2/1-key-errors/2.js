fbb146927a2ba8f1d1dd1e2bebf32f8aabfcf155
olhakoreniuk-debug <olha.koreniuk@gmail.com>
2026-01-30 17:56:39 +0100

    Completed exercises 1-key-errors

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,14 +4,19 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+// I think it will be SyntaxError: Unexpected number because instead of variable it was written a value 3.
 
-function square(3) {
+function square(num) {
     return num * num;
 }
+console.log(square(8888));
+
 
 // =============> write the error message here
+// SyntaxError: Unexpected number
 
 // =============> explain this error message here
+// Instead of variable it was written a value 3. I changed 3 to num to fix the error.
 
 // Finally, correct the code to fix the problem
 
